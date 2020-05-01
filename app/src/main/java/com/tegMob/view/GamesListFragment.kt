a2b0db40ce90a93ac1207b6d99eb45797700afea
Juan Göttling <juangottling@gmail.com>
2020-05-01 14:02:01 -0300

    added games list screen

---
+++ app/src/main/java/com/tegMob/view/GamesListFragment.kt
@@ -0,0 +1,19 @@
+package com.tegMob.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tegMob.R
+
+class GamesListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.games_list_fragment, container, false)
+    }
+
+}
