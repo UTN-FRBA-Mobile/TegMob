8d36e561e71ff83e9f0139f2af503d7b53619c4c
Juan Göttling <juangottling@gmail.com>
2020-04-30 21:55:33 -0300

    new screen

---
+++ app/src/main/java/com/tegMob/view/LoggedUserMainFragment.kt
@@ -0,0 +1,18 @@
+package com.tegMob.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tegMob.R
+
+class LoggedUserMainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.logged_user_main_fragment, container, false)
+    }
+}
