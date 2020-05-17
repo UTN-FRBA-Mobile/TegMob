8a70d54c6930d000bf4df229a78ec0e918195b62
Guille <gafernandez2525@gmail.com>
2020-05-17 19:42:35 -0300

    mapa con sudamérica y africa viéndose bien y detectando bien los toques de pantalla

--- app/src/main/java/com/tegMob/view/MapFragment.kt
+++ app/src/main/java/com/tegMob/view/MapFragment.kt
@@ -20,9 +20,8 @@ import kotlinx.android.synthetic.main.map_fragment.*
 import kotlinx.android.synthetic.main.map_fragment.view.*
 
 
-
 class MapFragment : Fragment() {
-//
+    //
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        arguments?.let {
@@ -40,27 +39,32 @@ class MapFragment : Fragment() {
         "25543189" to "Uruguay",
         "132084" to "Sahara",
         "25400" to "Egipto",
-        "2520255" to "Etipoía",
+        "2520255" to "Etiopía",
         "2342550" to "Zaire",
         "302540" to "Sudáfrica",
         "971330" to "Madagascar"
     )
 
+    private var chileColors = "pink"
+
     private lateinit var bitMapFullView: Bitmap
-//    private lateinit var fullView: View
-    private var windowHeight: Int=0
-    private var windowWidth: Int=0
-    private var densityRelation:Float = 0F
+
+    //    private lateinit var fullView: View
+    private var windowHeight: Int = 0
+    private var windowWidth: Int = 0
+    private var densityRelation: Float = 0F
     private val displayMetrics = DisplayMetrics()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
 //        return inflater.inflate(R.layout.main_fragment, container, false)
         val view: View = inflater.inflate(R.layout.map_fragment, container, false)
         view.setOnTouchListener { v, event ->
             if (event.action == ACTION_DOWN) {
                 //do something
-                screenTouched(v,event)
+                screenTouched(v, event)
             }
             true
         }
@@ -70,13 +74,13 @@ class MapFragment : Fragment() {
         windowHeight = displayMetrics.heightPixels
         densityRelation = displayMetrics.density
 //        fullView=view
-        bitMapFullView=loadBitmapFromView(view,windowWidth,windowHeight)
+        bitMapFullView = loadBitmapFromView(view, windowWidth, windowHeight)
 
         return view
     }
 
     private fun loadBitmapFromView(v: View, width: Int, height: Int): Bitmap {
-        val b = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888)
+        val b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val c = Canvas(b)
         v.layout(0, 0, width, height)
         //Get the view’s background
@@ -90,109 +94,74 @@ class MapFragment : Fragment() {
     }
 
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val widthRelation:Float= windowWidth/800F
-        val heightRelation:Float= windowHeight/480F
+        val widthRelation: Float = windowWidth / 800F
+        val heightRelation: Float = windowHeight / 480F
 
         //América del Sur
-        imageColombia.layoutParams.width=(70*widthRelation).toInt()
-        imageColombia.layoutParams.height=(47*heightRelation).toInt()
-        imagePeru.layoutParams.width=(63*widthRelation).toInt()
-        imagePeru.layoutParams.height=(42*heightRelation).toInt()
-        imageBrasil.layoutParams.width=(106*widthRelation).toInt()
-        imageBrasil.layoutParams.height=(87*heightRelation).toInt()
-        imageChile.layoutParams.width=(20*widthRelation).toInt()
-        imageChile.layoutParams.height=(90*heightRelation).toInt()
-        imageArgentina.layoutParams.width=(50*widthRelation).toInt()
-        imageArgentina.layoutParams.height=(90*heightRelation).toInt()
-        imageUruguay.layoutParams.width=(49*widthRelation).toInt()
-        imageUruguay.layoutParams.height=(44*heightRelation).toInt()
+        //Tamaño de los países
+        imageColombia.layoutParams.width = (67 * widthRelation).toInt()
+        imageColombia.layoutParams.height = (50 * heightRelation).toInt()
+        imagePeru.layoutParams.width = (69 * widthRelation).toInt()
+        imagePeru.layoutParams.height = (46 * heightRelation).toInt()
+        imageBrasil.layoutParams.width = (102 * widthRelation).toInt()
+        imageBrasil.layoutParams.height = (89 * heightRelation).toInt()
+        imageChile.layoutParams.width = (23 * widthRelation).toInt()
+        imageChile.layoutParams.height = (87 * heightRelation).toInt()
+        imageArgentina.layoutParams.width = (50 * widthRelation).toInt()
+        imageArgentina.layoutParams.height = (91 * heightRelation).toInt()
+        imageUruguay.layoutParams.width = (52 * widthRelation).toInt()
+        imageUruguay.layoutParams.height = (44 * heightRelation).toInt()
 
-        imageChile.setOnClickListener {
-            onCountryTouched(it as ImageView)
-        }
-//
-//        imageColombiaBack.layoutParams.width=imageColombia.layoutParams.width
-//        imageColombiaBack.layoutParams.height=imageColombia.layoutParams.height
-//        imagePeruBack.layoutParams.width=imagePeru.layoutParams.width
-//        imagePeruBack.layoutParams.height=imagePeru.layoutParams.height
-//        imageBrasilBack.layoutParams.width=imageBrasil.layoutParams.width
-//        imageBrasilBack.layoutParams.height=imageBrasil.layoutParams.height
-//        imageChileBack.layoutParams.width=imageChile.layoutParams.width
-//        imageChileBack.layoutParams.height=imageChile.layoutParams.height
-//        imageArgentinaBack.layoutParams.width=imageArgentina.layoutParams.width
-//        imageArgentinaBack.layoutParams.height=imageArgentina.layoutParams.height
-//        imageUruguayBack.layoutParams.width=imageUruguay.layoutParams.width
-//        imageUruguayBack.layoutParams.height=imageUruguay.layoutParams.height
+        //Ubicación de los países
+        imageColombia.x = 217F * widthRelation
+        imageColombia.y = 243F * heightRelation     //243
+        imagePeru.x = 224F * widthRelation
+        imagePeru.y = 282F * heightRelation
+        imageBrasil.x = 269F * widthRelation    //268
+        imageBrasil.y = 246F * heightRelation
+        imageChile.x = 238F * widthRelation     //238
+        imageChile.y = 321F * heightRelation
+        imageArgentina.x = 247F * widthRelation
+        imageArgentina.y = 321F * widthRelation
+        imageUruguay.x = 285F * widthRelation   //286
+        imageUruguay.y = 322F * widthRelation   //322
 
-        imageColombia.x=220F*widthRelation
-        imageColombia.y=245F*heightRelation
-        imagePeru.x=225F*widthRelation
-        imagePeru.y=285F*heightRelation
-        imageBrasil.x=270F*widthRelation
-        imageBrasil.y=250F*heightRelation
-        imageChile.x=235F*widthRelation
-        imageChile.y=320F*heightRelation
-        imageArgentina.x=245F*widthRelation
-        imageArgentina.y=imageChile.y
-        imageUruguay.x=285F*widthRelation
-        imageUruguay.y=imageChile.y
-
-//        imageColombiaBack.x=imageColombia.x
-//        imageColombiaBack.y=imageColombia.y
-//        imagePeruBack.x=imagePeru.x
-//        imagePeruBack.y=imagePeru.y
-//        imageBrasilBack.x=imageBrasil.x
-//        imageBrasilBack.y=imageBrasil.y
-//        imageChileBack.x=imageChile.x
-//        imageChileBack.y=imageChile.y
-//        imageArgentinaBack.x=imageArgentina.x
-//        imageArgentinaBack.y=imageArgentina.y
-//        imageUruguayBack.x=imageUruguay.x
-//        imageUruguayBack.y=imageUruguay.y
-
-        // África
-        imageSahara.layoutParams.width=(100*widthRelation).toInt()
-        imageSahara.layoutParams.height=(75*heightRelation).toInt()
-        imageEgipto.layoutParams.width=(140*widthRelation).toInt()
-        imageEgipto.layoutParams.height=(70*heightRelation).toInt()
-        imageEtiopia.layoutParams.width=(120*widthRelation).toInt()
-        imageEtiopia.layoutParams.height=(50*heightRelation).toInt()
-        imageZaire.layoutParams.width=(100*widthRelation).toInt()
-        imageZaire.layoutParams.height=(59*heightRelation).toInt()
-        imageSudafrica.layoutParams.width=(64*widthRelation).toInt()
-        imageSudafrica.layoutParams.height=(69*heightRelation).toInt()
-
-        imageSahara.x=440F*widthRelation
-        imageSahara.y=295F*heightRelation
-//        imageSahara.setOnClickListener {
+//        imageChile.setOnClickListener {
 //            onCountryTouched(it as ImageView)
 //        }
-        imageEgipto.x=505F*widthRelation
-        imageEgipto.y=290F*heightRelation
-//        imageEgipto.setOnClickListener {
-//            onCountryTouched(it)
-//        }
-        imageEtiopia.x=505*widthRelation
-        imageEtiopia.y=320F*heightRelation
-//        imageEtiopia.setOnClickListener {
-//            onCountryTouched(it)
-//        }
-        imageZaire.x=475F*widthRelation
-        imageZaire.y=340F*heightRelation
-//        imageZaire.setOnClickListener {
-//            onCountryTouched(it)
-//        }
-        imageSudafrica.x=540F*widthRelation
-        imageSudafrica.y=360F*heightRelation
-//        imageSudafrica.setOnClickListener {
-//            onCountryTouched(it)
-//        }
 
+
+        // África
+        //Tamaño de los países
+        imageSahara.layoutParams.width = (90 * widthRelation).toInt()
+        imageSahara.layoutParams.height = (80 * heightRelation).toInt()
+        imageEgipto.layoutParams.width = (126 * widthRelation).toInt()  //126
+        imageEgipto.layoutParams.height = (58 * heightRelation).toInt() //58
+        imageEtiopia.layoutParams.width = (93 * widthRelation).toInt()
+        imageEtiopia.layoutParams.height = (54 * heightRelation).toInt()
+        imageZaire.layoutParams.width = (126 * widthRelation).toInt()
+        imageZaire.layoutParams.height = (58 * heightRelation).toInt()
+        imageSudafrica.layoutParams.width = (70 * widthRelation).toInt()
+        imageSudafrica.layoutParams.height = (65 * heightRelation).toInt()
+        imageMadagascar.layoutParams.width = (45 * widthRelation).toInt()
+        imageMadagascar.layoutParams.height = (118 * heightRelation).toInt()
+
+        //Ubicación de los países
+        imageSahara.x = 448F * widthRelation    //450
+        imageSahara.y = 294F * heightRelation
+        imageEgipto.x = 517F * widthRelation    //520
+        imageEgipto.y = 289F * heightRelation
+        imageEtiopia.x = 518 * widthRelation
+        imageEtiopia.y = 317F * heightRelation  //330
+        imageZaire.x = 464F * widthRelation
+        imageZaire.y = 340F * heightRelation
+        imageSudafrica.x = 537F * widthRelation
+        imageSudafrica.y = 364F * heightRelation
+        imageMadagascar.x = 618F * widthRelation
+        imageMadagascar.y = 317F * heightRelation
 
     }
 
@@ -200,18 +169,49 @@ class MapFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
     }
 
-    private fun screenTouched(view: View,event: MotionEvent): Boolean {
+    private fun screenTouched(view: View, event: MotionEvent): Boolean {
 
         val touchColor: Int = bitMapFullView.getPixel(event.getX().toInt(), event.getY().toInt())
         val redValue = Color.red(touchColor)
         val blueValue = Color.blue(touchColor)
         val greenValue = Color.green(touchColor)
-        val countryName:String?=countryBackColors[redValue.toString()+blueValue.toString()+greenValue.toString()]
-        if(countryName!=null)
+        val countryName: String? =
+            countryBackColors[redValue.toString() + blueValue.toString() + greenValue.toString()]
+        if (countryName != null)
             AlertDialog.Builder(activity!!)
-                .setTitle("País: "+countryName)
+                .setTitle("País: " + countryName)
                 .setPositiveButton("OK") { _, _ -> }
                 .create().show()
+
+        if (countryName == "Chile") {
+            when (chileColors) {
+                "pink" -> {
+                    imageChile.setImageResource(R.drawable.chile_light_blue)
+                    chileColors = "lightBlue"
+                }
+                "lightBlue" -> {
+                    imageChile.setImageResource(R.drawable.chile_black)
+                    chileColors = "black"
+                }
+                "black" -> {
+                    imageChile.setImageResource(R.drawable.chile_green)
+                    chileColors = "green"
+                }
+                "green" -> {
+                    imageChile.setImageResource(R.drawable.chile_yellow)
+                    chileColors = "yellow"
+                }
+                "yellow" -> {
+                    imageChile.setImageResource(R.drawable.chile_red)
+                    chileColors = "red"
+                }
+                "red" -> {
+                    imageChile.setImageResource(R.drawable.chile_pink)
+                    chileColors = "pink"
+                }
+            }
+
+        }
 //        AlertDialog.Builder(activity!!)
 //            .setTitle(redValue.toString()+blueValue.toString()+greenValue.toString())
 //            .setPositiveButton("OK") { _, _ -> }
@@ -220,7 +220,7 @@ class MapFragment : Fragment() {
         return true
     }
 
-    private fun onCountryTouched(country:ImageView){
+    private fun onCountryTouched(country: ImageView) {
         country.setImageResource(R.drawable.chile_light_blue)
     }
 
